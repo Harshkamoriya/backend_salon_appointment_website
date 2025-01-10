979dc411adcf248b2ff080defcbefff428ce45be
Harshkamoriya <kamoriyaharsh@gmail.com>
2025-01-10 22:23:12 +0530

    updated server.js file

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import cors from "cors"
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5173" // Change this to your frontend URL
+    origin: "https://salonease-oy0f.onrender.com" // Change this to your frontend URL
 }));
 
 app.use(express.json());
