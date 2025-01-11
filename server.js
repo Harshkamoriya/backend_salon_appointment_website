ee0436ff9c67cef65b0637d7b3a01806f1b1c727
Harshkamoriya <kamoriyaharsh@gmail.com>
2025-01-11 13:06:57 +0530

    changed the cors url

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import cors from "cors"
 
 const app = express();
 app.use(cors({
-    origin: "https://frontend-salon-appointment-website.vercel.app" // Change this to your frontend URL
+    origin: "https://salonease.vercel.app" // Change this to your frontend URL
 }));
 
 app.use(express.json());
