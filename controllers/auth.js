9b224c8c1b0467d491e593f5e58659dfa43df6f0
Harshkamoriya <kamoriyaharsh@gmail.com>
2025-01-15 14:40:19 +0530

    changed the owner

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,7 +30,7 @@ const signup = async (req, res) => {
         });
 
         // Set role as 'admin' if the email matches the salon owner's email
-        if (email === 'admin@example.com') {
+        if (email === 'owner@example.com') {
             newUser.role = 'admin';
         }
 
